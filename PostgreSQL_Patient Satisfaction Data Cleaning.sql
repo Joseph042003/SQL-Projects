6334cfcb07a4c2f2230b4849634f67d33174c044
Joseph042003 <phusangtran03@gmail.com>
2024-11-12 18:44:52 +1100

    PostgreSQL_Patient Satisfaction Data Cleaning.sql

--- PostgreSQL_Patient Satisfaction Data Cleaning.sql
+++ PostgreSQL_Patient Satisfaction Data Cleaning.sql
@@ -1,6 +1,6 @@
 /*
 Project: Patient Satisfaction Survey Data Cleaning in PostgreSQL 
-Skilled Used: CTEs, JOIN, TO_DATE, CAST
+Skilled Used: CTEs, JOIN, TO_DATE FUNCTION TO STANDARDISE DATE, CAST, AGGREGRATE FUNCTIONS
 
 */
 -- Checking the data after importing into Postgres
@@ -12,28 +12,28 @@ Skilled Used: CTEs, JOIN, TO_DATE, CAST
 
 
 --Creating common table expression 
-with hospital_beds_prep as 
+WITH hospital_beds_prep AS 
 (
-select lpad(cast(provider_ccn as text),6,'0') as provider_ccn, -- convert to text and left padding number 0 until reaching 6 characters.
+SELECT lpad(CAST(provider_ccn AS text),6,'0') AS provider_ccn, -- convert to text and left padding number 0 until reaching 6 characters.
  	   hospital_name,
-	   to_date(fiscal_year_begin_date,'MM/DD/YYYY') as fiscal_year_begin_date, -- change the format of date from text to date 
+	   to_date(fiscal_year_begin_date,'MM/DD/YYYY') AS fiscal_year_begin_date, -- change the format of date from text to date 
 	   									   -- and modify the date to MM/DD/YYYY format								  
-	   to_date(fiscal_year_end_date,'MM/DD/YYYY') as fiscal_year_end_date,
+	   to_date(fiscal_year_end_date,'MM/DD/YYYY') AS fiscal_year_end_date,
 	   number_of_beds,
 	   -- assigning number to the hospitals that are reported more than once by the provider_cnn value, 
 	   -- the newest reporting date will be assigned as 1 and so on.
-	   row_number() over (partition by provider_ccn order by to_date(fiscal_year_end_date,'MM/DD/YYYY') desc) as nth_row
-from "postgres"."hospital_Data".hospital_beds
+	   ROW_NUMBER() OVER (PARTITION BY provider_ccn ORDER BY  to_date(fiscal_year_end_date,'MM/DD/YYYY') DESC) AS nth_row
+FROM "postgres"."hospital_Data".hospital_beds
 )
 
-select lpad(cast(facility_id as text),6,'0') as provider_ccn, 
-	   to_date(start_date,'MM/DD/YYYY') as start_date_converted, 							  
-	   to_date(end_date,'MM/DD/YYYY') as end_date_converted,
+SELECT lpad(CAST(facility_id AS text),6,'0') AS provider_ccn, 
+	   to_date(start_date,'MM/DD/YYYY') AS start_date_converted, 							  
+	   to_date(end_date,'MM/DD/YYYY') AS end_date_converted,
 	   hcahps.*,
 	   beds.number_of_beds, 
-	   beds.fiscal_year_begin_date as beds_start_report_period, 
-	   beds.fiscal_year_end_date as beds_end_report_period
-from "postgres"."hospital_Data".hcahps_data as hcahps
-left join hospital_beds_prep as beds
-on lpad(cast(facility_id as text),6,'0') = beds.provider_ccn
-and beds.nth_row = 1
+	   beds.fiscal_year_begin_date AS beds_start_report_period, 
+	   beds.fiscal_year_end_date AS beds_end_report_period
+FROM "postgres"."hospital_Data".hcahps_data AS hcahps
+LEFT JOIN hospital_beds_prep AS beds
+ON lpad(CAST(facility_id as text),6,'0') = beds.provider_ccn
+AND beds.nth_row = 1
